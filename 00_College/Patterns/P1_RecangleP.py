56ec7060b578997f2cb8d1b642b7eb612d859735
Shravanis30 <salunkeshravani561@gmail.com>
2025-02-18 19:10:22 +0530

    Pattern rectangle

---
+++ 00_College/Patterns/P1_RecangleP.py
@@ -0,0 +1,16 @@
+    # *****
+    # *****
+    # *****
+    # *****
+    
+
+num = int(input("Enter the number : "))
+i = 0
+while i < num :
+    j = 0
+    while j < num :
+        print("*", end=" ")
+        j+=1
+    print()
+    i+=1
+    

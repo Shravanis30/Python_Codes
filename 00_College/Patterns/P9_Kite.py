42bdea17a3f778d2e61735fc0ec218b1e8567502
Shravanis30 <salunkeshravani561@gmail.com>
2025-02-27 22:00:03 +0530

    Kite pattern

---
+++ 00_College/Patterns/P9_Kite.py
@@ -0,0 +1,14 @@
+#     *
+#    ***
+#   *****
+#  *******
+#   *****
+#    ***
+#     *
+
+num = int(input("Enter the value of num : "))
+
+i = 1
+while i <= 2 * num - 1:
+    j = 1
+    while j >= num + i -1
